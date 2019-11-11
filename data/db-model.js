75357fb90729004b1dd5abc87ad3f0250c3a6bae
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-11 14:14:00 +0100

    functions initialized

---
+++ data/db-model.js
@@ -0,0 +1,21 @@
+const db = require("../data/db-config.js");
+
+
+module.exports = {
+    register,
+    login,
+    getUsers,
+}
+
+
+function register() {
+
+}
+
+function login() {
+
+}
+
+function getUsers() {
+    
+}
