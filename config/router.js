c5452a43aefb514474900015d64cabbba048699c
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-12 15:23:52 +0100

    MVP reached; stretch attempted

--- config/router.js
+++ config/router.js
@@ -1,37 +1,12 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
 const model = require("../data/db-model");
+const restricted = require("../auth/restricted-middleware.js");
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", restricted, login);
-router.get("/users",restricted, showUsers);
-
-function register(req, res) {
-  const { name, password } = req.body;
-  const hash = bcrypt.hashSync(password, 11);
-
-  const newUser = {
-    name: name,
-    password: hash
-  };
-
-  model
-    .register(newUser)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
-}
-
-function login(req, res) {
-  const { name} = req.headers;
-  res.status(200).json({ message: `Welcome ${name}!` });
-}
+router.get("/users", restricted, showUsers);
 
 function showUsers(req, res) {
   model
@@ -43,24 +18,5 @@ function showUsers(req, res) {
       res.status(500).json(err);
     });
 }
-
-
-
-//middleware
-function restricted(req, res, next) {
-    const {name, password} = req.headers
-    model.login({name})
-    .first()
-    .then(user => {
-      if(user && bcrypt.compareSync(password, user.password)){
-        next()
-      } else{
-        res.status(401).json({message: 'Invalid Credentials'})
-      }
-    })
-    .catch(error => {
-      res.status(500).json({message: error.message})
-    })
-  }
 
 module.exports = router;
